6e25542fe0041923ee415cedb3f62735f69ed499
amwa7066 <amwa7066@colorado.edu>
2025-09-30 21:38:56 -0600

    6

--- wa/week6/community/w6.js
+++ wa/week6/community/w6.js
@@ -1,15 +1,18 @@
+/* NAV TOGGLE */
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
-navToggle.addEventListener('click', () => {
-  const expanded = navToggle.getAttribute('aria-expanded') === 'true';
-  navToggle.setAttribute('aria-expanded', !expanded);
-  navMenu.classList.toggle('show');
-});
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    const expanded = navToggle.getAttribute('aria-expanded') === 'true';
+    navToggle.setAttribute('aria-expanded', !expanded);
+    navMenu.classList.toggle('show');
+  });
 
-navToggle.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter' || event.key === ' ') {
-    event.preventDefault();
-    navToggle.click();
-  }
-});
+  navToggle.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      navToggle.click();
+    }
+  });
+}
